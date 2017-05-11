bf4784bfcf6dc869e36e904a309ba17822bb33b5
MHFS20 <matheus.181137@gmail.com>
2017-05-11 11:13:47 -0300

    segundo

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-SECURE_PROXY_SSL_HEADER =('HTTP_X_FOWARDED_PROTO', 'https')
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
